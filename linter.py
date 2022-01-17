b7d8a44d91de0415aeb85da86c7701c9d5b28643
Alexander <alexkuz@gmail.com>
2022-01-17 03:30:25 +0300

    run zig ast-check

--- linter.py
+++ linter.py
@@ -1,10 +1,12 @@
-from SublimeLinter.lint import Linter  # or NodeLinter, PythonLinter, ComposerLinter, RubyLinter
+from SublimeLinter.lint import Linter, STREAM_STDERR
 
 
-class __class__(Linter):
-    cmd = '__cmd__'
-    regex = r''
+class SublimeLinterZigCheck(Linter):
+    name = 'zig ast-check'
+    cmd = ('zig', 'ast-check', '--color', 'off')
+    regex = r'^<stdin>:(?P<line>\d+):(?P<col>\d+):\s+error:\s+(?P<message>.*)$'
     multiline = False
     defaults = {
-        'selector': 'source.python'
+        'selector': 'source.zig'
     }
+    error_stream = STREAM_STDERR
