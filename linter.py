34ba16be0984d0cd1663ee3b4c43d7e7331fa2e5
Alexander <alexkuz@gmail.com>
2022-01-20 03:10:32 +0300

    remove redundant error filtering

--- linter.py
+++ linter.py
@@ -12,25 +12,6 @@ class SublimeLinterZigCheck(Linter):
     }
     error_stream = STREAM_STDERR
 
-    def split_match(self, match):
-        filename = match.group('filename')
-        values = super().split_match(match)
-
-        if self.is_stdin_filename(filename):
-            return values
-
-        if not os.path.isabs(filename):
-            cwd = self.get_working_dir() or os.getcwd()
-            filename = os.path.join(cwd, filename)
-
-        filename = os.path.normpath(filename)
-
-        if (filename != self.view.file_name()):
-            return None
-
-        return values
-
-
     def cmd(self):
         build_cmd = self.settings.get("build-cmd")
         if build_cmd and not self.view.is_dirty():
